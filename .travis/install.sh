823d9fde3a4c91bad1f904e993a6cfd9819a2016
Alexander Kurbatov <Alexander.Kurbatov@acronis.com>
2020-05-18 09:52:16 +0300

    build: Refine CI routine
    * Drop configure step as all the env variables set required only at the
    build time. Now it is simpler to identify what with build with which
    tool.
    * Replace 'skip_cleanup = true' with 'cleanup = false' according to the
    latest changes in Travis.
    * Test against Ubuntu bionic and its default compiler, seems that we
    have no need in the latest gcc so far.
    * Move linux packages installation to the install script to keep all
    things in one place.
    * Fix minor Travis warnings regarding token and strategy.
    * Remove comments.
    
    Signed-off-by: Alexander Kurbatov <Alexander.Kurbatov@acronis.com>

--- .travis/install.sh
+++ .travis/install.sh
@@ -9,5 +9,10 @@ set -ev
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
   choco install visualstudio2019community
   choco install visualstudio2019-workload-nativedesktop
+elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+  sudo apt-get -qq update
+
+  sudo apt-get install -y doxygen
 fi
 
