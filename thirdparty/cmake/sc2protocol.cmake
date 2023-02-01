5e318565cef915ab3bb6650eba7c91c761da7bc4
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2023-02-01 10:15:36 +0300

    feat: Rebase to the latest proto
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- thirdparty/cmake/sc2protocol.cmake
+++ thirdparty/cmake/sc2protocol.cmake
@@ -3,6 +3,6 @@ message(STATUS "FetchContent: protocol")
 FetchContent_Declare(
     sc2protocol
     GIT_REPOSITORY https://github.com/Blizzard/s2client-proto.git
-    GIT_TAG 18996243c8c51e431fae177a7f3d0a65d12e9365
+    GIT_TAG 42f4641080f421119ad8214d8a252b3797c09ca9
 )
 FetchContent_MakeAvailable(sc2protocol)
