e6f9b763901d37ae31c5ebf61d227efc83d49c4a
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2023-10-11 23:21:40 +0300

    fix: Revert changes in civetweb because of broken Windows
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- thirdparty/cmake/civetweb.cmake
+++ thirdparty/cmake/civetweb.cmake
@@ -16,7 +16,7 @@ set(CIVETWEB_ENABLE_IPV6 OFF CACHE BOOL "" FORCE)
 FetchContent_Declare(
     civetweb
     GIT_REPOSITORY https://github.com/cpp-sc2/civetweb.git
-    GIT_TAG d814cc25f56ab9468863e27410a33146301594f6
+    GIT_TAG 61fc8f811acb3da60d313d5459aa93c9e85fe32c
 )
 FetchContent_MakeAvailable(civetweb)
 
