3c6215a0ac064d48bd3f5f666cfd1597a2ea96b8
Nick Rader <nick.rader@yahoo.com>
2024-08-13 08:25:48 -0600

    build: Link precompiled protobuf compiler (protoc) for WSL builds.

--- cmake/assets/protos.cmake
+++
@@ -1,8 +0,0 @@
-message(STATUS "FetchContent: generated protos for Windows")
-
-FetchContent_Declare(
-    protos
-    URL https://storage.yandexcloud.net/cpp-sc2/wsl2/generated-s2clientprotocol-db14236-protobuf-1a74ba4.zip
-    URL_HASH MD5=346e3d8127f2da6cea4900465183314b
-)
-FetchContent_MakeAvailable(protos)
--- thirdparty/cmake/protobuf.cmake
+++ thirdparty/cmake/protobuf.cmake
@@ -3,7 +3,7 @@ message(STATUS "FetchContent: protobuf")
 set(protobuf_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set(protobuf_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 
-# Do not build Protobuf compiler if using precompiled proto files
+# Cross compiling causes protoc to be built for target (Windows)
 if (WSL2_CROSS_COMPILE)
     set(protobuf_BUILD_PROTOC_BINARIES OFF CACHE BOOL "" FORCE)
 endif ()
@@ -15,7 +15,19 @@ FetchContent_Declare(
 )
 FetchContent_MakeAvailable(protobuf)
 
-set(protobuf_targets libprotobuf libprotobuf-lite libprotoc protoc)
+if (WSL2_CROSS_COMPILE)
+    message(STATUS "FetchContent: pre-compiled protoc binary for WSL")
+    # This must version match protobuf GIT_TAG above, or will generate runtime errors.
+    FetchContent_Declare(
+        protos
+        URL https://github.com/protocolbuffers/protobuf/releases/download/v23.4/protoc-23.4-linux-x86_64.zip
+    )
+    FetchContent_MakeAvailable(protos)
+    set(protobuf_targets libprotobuf libprotobuf-lite libprotoc)
+
+else ()
+    set(protobuf_targets libprotobuf libprotobuf-lite libprotoc protoc)
+endif ()
 
 foreach (target IN LISTS protobuf_targets)
     if (TARGET ${target})
