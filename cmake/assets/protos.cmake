8bc55a936417107d9200ebd80ed7c565633bbb9e
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2024-09-19 18:14:45 +0300

    fix: Update WSL2 proto assets, #143
    To fix WSL2 build with the current protobuf.
    
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- cmake/assets/protos.cmake
+++ cmake/assets/protos.cmake
@@ -2,7 +2,7 @@ message(STATUS "FetchContent: generated protos for Windows")
 
 FetchContent_Declare(
     protos
-    URL https://storage.yandexcloud.net/cpp-sc2/wsl2/generated-s2clientprotocol-db14236-protobuf-1a74ba4.zip
-    URL_HASH MD5=346e3d8127f2da6cea4900465183314b
+    URL https://storage.yandexcloud.net/cpp-sc2/wsl2/generated-s2clientprotocol-998ca3b-protobuf-2c35697b.zip
+    URL_HASH MD5=1eb0a4e762d366dfc052194b691fc0ab
 )
 FetchContent_MakeAvailable(protos)
