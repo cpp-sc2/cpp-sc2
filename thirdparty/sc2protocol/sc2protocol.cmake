cdfe6d1189bac4b2cedba95aad9cbe48fcf32015
spaceghost1589 <sean.egan27@gmail.com>
2026-09-13 13:22:17 -0700

    Added a patch to sc2 .proto files to enable: - Packed Encoding for Repeated Fields - Arena Allocation

--- thirdparty/sc2protocol/sc2protocol.cmake
+++ thirdparty/sc2protocol/sc2protocol.cmake
@@ -5,5 +5,9 @@ FetchContent_Declare(
     GIT_REPOSITORY https://github.com/Blizzard/s2client-proto.git
     GIT_TAG bff45dae1fc685e6acbaae084670afb7d1c0832c
     GIT_PROGRESS TRUE
+    # Patch to apply:
+    # - Packed Encoding for Repeated Fields
+    # - Arena Allocation
+    PATCH_COMMAND git apply "${CMAKE_CURRENT_LIST_DIR}/sc2protocol-arena-packing.patch"
 )
 FetchContent_MakeAvailable(sc2protocol)
